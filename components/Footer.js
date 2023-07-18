7d8a5fd923c671f2e389806ffbfa22a06d4c3761
DevBigEazi <adesholatajudeen1@gmail.com>
2023-07-18 09:44:06 +0100

    Tablet and mobile view adjusted

--- components/Footer.js
+++ components/Footer.js
@@ -80,12 +80,15 @@ const Footer = () => {
           ))}
         </Box>
 
-        <Box>
+        <Box
+          display={{ base: "flex", sm: "none", md: "flex" }}
+          flexDir="column"
+        >
           <Heading as="h5" fontSize="xl" fontWeight="500">
             Join the community
           </Heading>
 
-          <Box display="flex">
+          <Box width="100%" display="flex">
             {community.map((item, i) => (
               <Text margin={2} fontSize="2xl" key={i}>
                 <Link display="inline" href={item.url} isExternal>
@@ -97,7 +100,37 @@ const Footer = () => {
         </Box>
       </Flex>
 
-      <Flex justifyContent="space-between" wrap="wrap" mt="20" gap="5">
+      {/* For Tablet */}
+      <Box
+        display={{ base: "none", sm: "flex", md: "none" }}
+        flexDir="column"
+        mt="20"
+        mr="8"
+        justifyContent="flex-end"
+        alignItems="flex-end"
+      >
+        <Heading as="h5" fontSize="xl" fontWeight="500">
+          Join the community
+        </Heading>
+
+        <Box width="100%" display="flex" justifyContent="flex-end">
+          {community.map((item, i) => (
+            <Text margin={2} fontSize="2xl" key={i}>
+              <Link display="inline-flex" href={item.url} isExternal>
+                {item.icon}
+              </Link>
+            </Text>
+          ))}
+        </Box>
+      </Box>
+
+      <Flex
+        display={{ base: "none", sm: "flex" }}
+        justifyContent="space-between"
+        wrap="wrap"
+        mt="20"
+        gap="5"
+      >
         <Text fontWeight="500" fontSize="lg">
           <Link href="#">Privacy Policy</Link>
         </Text>
@@ -108,6 +141,29 @@ const Footer = () => {
           <Link href="#">Terms of Service</Link>
         </Text>
       </Flex>
+
+      {/* For Mobile Phones and small screen */}
+      <Box
+        display={{ base: "flex", sm: "none" }}
+        flexDir="column"
+        justifyContent="center"
+        wrap="wrap"
+        mt="20"
+        gap="5"
+      >
+        <Text fontWeight="500" fontSize="lg" textAlign="center">
+          &copy; {currentYear} Celo Inc.
+        </Text>
+        <Flex justifyContent="space-between">
+          <Text fontWeight="500" fontSize="lg">
+            <Link href="#">Privacy Policy</Link>
+          </Text>
+
+          <Text fontWeight="500" fontSize="lg">
+            <Link href="#">Terms of Service</Link>
+          </Text>
+        </Flex>
+      </Box>
     </Container>
   );
 };
